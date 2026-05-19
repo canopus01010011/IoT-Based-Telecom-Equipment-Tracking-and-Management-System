29e03bdb8d74b5684c0a8f8cb4dc1ddf762d414f
rahimereghioui <rahimereghioui@gmail.com>
2026-05-19 11:49:18 +0100

    update gpx file paths for devices in config

--- iot_system/config.py
+++ iot_system/config.py
@@ -13,7 +13,7 @@ TOPIC_GPS = "ericsson/sites/{siteID}/{deviceID}/gps"
 GPS_INTERVAL = 10
 
 DEVICES = [
-    {'siteID': 'site_alger', 'deviceID': 'package_001', 'route': 'OS-Draria', 'gpx_file': 'roads/OS_Draria.gpx'},
-    {'siteID': 'site_alger', 'deviceID': 'package_002', 'route': 'OS-Meftah', 'gpx_file': 'roads/OS_Meftah.gpx'},
-    {'siteID': 'site_alger', 'deviceID': 'package_003', 'route': 'OS-Cheraga', 'gpx_file': 'roads/OS_Cheraga.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_001', 'route': 'OS-Draria', 'gpx_file': 'OS_Draria.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_002', 'route': 'OS-Meftah', 'gpx_file': 'OS_Meftah.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_003', 'route': 'OS-Cheraga', 'gpx_file': 'OS_Cheraga.gpx'},
 ]
