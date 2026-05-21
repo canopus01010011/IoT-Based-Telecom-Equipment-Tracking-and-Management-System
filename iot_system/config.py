58b7bd7e162f8b9c4c02bca1a6d5cd52b208cf4e
Nazim <nazimguerguadj@gmail.com>
2026-05-21 20:07:17 +0100

    Add files via upload

--- iot_system/config.py
+++ iot_system/config.py
@@ -18,4 +18,9 @@ DEVICES = [
     {'siteID': 'site_alger', 'deviceID': 'package_003', 'route': 'OS-Cheraga', 'gpx_file': 'roads/OS_Cheraga.gpx'},
     {'siteID': 'site_alger', 'deviceID': 'package_004', 'route': 'OS-Bouzareah', 'gpx_file': 'roads/OS_Bouzareah.gpx'},
     {'siteID': 'site_alger', 'deviceID': 'package_005', 'route': 'OS-BabaHassen', 'gpx_file': 'roads/OS_BabaHassen.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_006', 'route': 'OS-Souakria', 'gpx_file': 'roads/OS_Souakria.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_007', 'route': 'OS-APN', 'gpx_file': 'roads/OS_APN.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_008', 'route': 'OS-HusseinDey', 'gpx_file': 'roads/OS_HusseinDey.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_009', 'route': 'OS-Birtouta', 'gpx_file': 'roads/OS_Birtouta.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_010', 'route': 'OS-Sablettes', 'gpx_file': 'roads/OS_Sablettes.gpx'},
 ]
