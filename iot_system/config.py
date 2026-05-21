ec68fcb7aec9170d0e3baae652fb8828b47a2f68
Nazim <nazimguerguadj@gmail.com>
2026-05-21 17:35:38 +0100

    Add files via upload

--- iot_system/config.py
+++ iot_system/config.py
@@ -3,19 +3,19 @@ import os
 
 load_dotenv()
 
-BROKER_HOST = os.getenv("BROKER_HOST", "localhost")
-BROKER_PORT = int(os.getenv("BROKER_PORT", "1883"))
+BROKER_HOST = os.getenv("BROKER_HOST")
+BROKER_PORT = int(os.getenv("BROKER_PORT"))
 MQTT_USERNAME = os.getenv("MQTT_USERNAME")
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
-BACKEND_GPS_URL = os.getenv("BACKEND_GPS_URL", "http://localhost:5000/api/gps/iot")
-IOT_API_TOKEN = os.getenv("IOT_API_TOKEN")
 
 TOPIC_GPS = "ericsson/sites/{siteID}/{deviceID}/gps"
 
-GPS_INTERVAL = 5
+GPS_INTERVAL = 10
 
 DEVICES = [
     {'siteID': 'site_alger', 'deviceID': 'package_001', 'route': 'OS-Draria', 'gpx_file': 'roads/OS_Draria.gpx'},
     {'siteID': 'site_alger', 'deviceID': 'package_002', 'route': 'OS-Meftah', 'gpx_file': 'roads/OS_Meftah.gpx'},
     {'siteID': 'site_alger', 'deviceID': 'package_003', 'route': 'OS-Cheraga', 'gpx_file': 'roads/OS_Cheraga.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_004', 'route': 'OS-Bouzareah', 'gpx_file': 'roads/OS_Bouzareah.gpx'},
+    {'siteID': 'site_alger', 'deviceID': 'package_005', 'route': 'OS-BabaHassen', 'gpx_file': 'roads/OS_BabaHassen.gpx'},
 ]
